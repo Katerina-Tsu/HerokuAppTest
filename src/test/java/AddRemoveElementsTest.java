0466ccf1c1587412bd31aff48594013ecba57efa
Katerina-Tsu <k.tsupikova@outlook.com>
2021-02-03 17:12:01 +0300

    Created asserts. Done test.

--- src/test/java/AddRemoveElementsTest.java
+++ src/test/java/AddRemoveElementsTest.java
@@ -12,21 +12,32 @@ public class AddRemoveElementsTest {
     private static final String HEROKU_APP_URL = "http://the-internet.herokuapp.com/add_remove_elements/";
 
     @BeforeMethod
-    public void  iniTTest(){
+    public void iniTTest() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-}
+    }
 
     @Test
     public void addRemoveElementsTest() {
         driver.get(HEROKU_APP_URL);
+        int countOne = driver.findElements(By.xpath("//*[@id=\"elements\"]/button")).size();
+        if (countOne > 0) {
+            System.out.println(countOne);
+        } else {
+            System.out.println("You have null buttons." + countOne);
+        }
         driver.findElement(By.xpath("//button[text()='Add Element']")).click();
         driver.findElement(By.xpath("//button[text()='Add Element']")).click();
         driver.findElement(By.xpath("//*[@onclick='deleteElement()']")).click();
-
-
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        int countTwo = driver.findElements(By.xpath("//*[@id=\"elements\"]/button")).size();
+        if (countTwo == 1) {
+            System.out.println("You have some Delete buttons:" + countTwo);
+        } else {
+            System.out.println("Mistake.");
+        }
         driver.quit();
-}
+    }
 }
